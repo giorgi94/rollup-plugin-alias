5a857f38258e15daf5bdf53ee1eacb0ebf8318ac
giorgi94 <giorgik1994@gmail.com>
2019-08-18 16:08:50 +0400

    added plugin description

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
-export { alias } from './rollup-plugin-alias'
+import fs from "fs";
+import path from "path";
+
+
+const isFile = (p) => {
+    try {
+        return fs.statSync(p).isFile();
+    } catch (e) {
+        return false;
+    }
+};
+
+const isDirectory = (p) => {
+    try {
+        return fs.statSync(p).isDirectory();
+    } catch (e) {
+        return false;
+    }
+};
+
+
+export default function alias({ entry = {}, extensions = [".js"] }) {
+
+    const entryKeys = Object.keys(entry);
+
+    if (!entryKeys.length) {
+        return {
+            resolveId: () => null,
+        };
+    }
+
+    return {
+        name: "alias",
+        resolveId(source) {
+
+            const key = entryKeys.find(key => source.startsWith(key));
+
+            if (!key || !source) {
+                return null;
+            }
+
+            source = source.replace(key, entry[key]);
+
+            if (isFile(source)) {
+                return source;
+            }
+
+            if (isDirectory(source)) {
+                return path.join(source, "index.js");
+            }
+
+            const filepaths = extensions.map(ext => source + ext);
+
+            for (let fpath of filepaths) {
+                if (isFile(fpath)) {
+                    return fpath;
+                }
+            }
+
+            return source;
+        },
+    };
+}
